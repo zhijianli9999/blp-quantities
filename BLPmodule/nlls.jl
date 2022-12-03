ed2423566fdc50031dfced237f6c98e709ed0e2b
Li Zhijian <zhijianli9999@gmail.com>
2022-12-03 14:35:44 -0800

    sample construction, some work on jump

--- BLPmodule/nlls.jl
+++ BLPmodule/nlls.jl
@@ -1,20 +1,27 @@
 
-function pred_q(θ::Vector{Float64}, n_firms::Int, tracts::Vector{Tract})
+function pred_q(θ::Vector, n_firms::Int, tracts::Vector{Tract})
+    # @showln θ
     # θ should have X before D
     KX = size(tracts[1].X)[2] 
     θX = θ[1:KX]
     θD = θ[(KX+1):end]
+    println(θX, θD)
     q_vec = zeros(n_firms)
-    Threads.@threads for tt in eachindex(tracts)
+    for tt in eachindex(tracts)
         t = tracts[tt]
-        @unpack firms, q, M, utils, inds, X, D, denom, shares = t
-        utils .= reduce(+, X * θX) + reduce(+, D * θD)
+        @unpack firms, n_firms, q, M, utils, inds, X, D, denom, shares = t
+        for jj in 1:n_firms
+            utils[jj,1] = reduce(+, X[jj,:] .* θX) + reduce(+, D[jj,:] .* θD)
+        end
         utils .= exp.(utils)
+        # @showln sum(utils, dims=1)
         denom .= 1 .+ sum(utils, dims=1)
         shares .= utils ./ denom
-        q .= vec(shares .* M)
-        view(q_vec, inds) .+= q
+        q .= shares .* M
+        q_vec[inds] .+= vec(q)
     end
+    # @showln q_vec
+    # error()
     return q_vec
 end
 
@@ -24,3 +31,17 @@ function nlls_obj(θ2, n_firms, tracts, q_obs)
     obj = reduce(+, (q_obs .- q_pred).^2)
     return obj
 end
+
+function predqt(
+    θ, 
+    X,
+    M
+    )
+    nJ = size(X)[1]
+    utils .= exp.([dot(X[jj,:], θ) for jj in 1:nJ])
+    shares .= utils ./ (1 .+ sum(utils, dims=1))
+    q .= shares .* M
+
+end
+
+
---
+++ datawork/genvars_old.do
@@ -0,0 +1,52 @@
+use $idir/sample_novars${testtag}.dta, clear
+
+
+***
+//scale factor - fraction of elderly population considered to be in market
+//for now, set as 0.1...
+// or 0.2 if tract has high residents:population ratio - avoid estimation issues
+
+
+bys facid: egen pop65_j = total(pop65plus_int) //population>65 in tracts near j
+gen ratio_occpop = restot / pop65_j
+sum ratio_occpop, d
+gen aux_highnhratio = ratio_occpop > `r(p95)'
+gen fracpop_inmkt = 0.1
+bys tractid: egen aux_boosttractpop = max(aux_highnhratio)
+replace fracpop_inmkt = 0.2 if aux_boosttractpop == 1
+drop aux_* ratio_occpop
+gen mktpop = fracpop_inmkt * pop65plus_int
+
+*** medicare residents
+gen nres_mcare = paymcare / 100 * restot
+drop paymcare
+
+
+sum mktpop, d
+
+// roughly tabulate total inside share
+preserve 
+collapse (first) mktpop fracpop_inmkt, by(tractid)
+tabstat mktpop, stat(sum)
+sum fracpop_inmkt, d 
+restore 
+preserve 
+collapse (first) restot, by(facid)
+tabstat restot, stat(sum)
+restore
+
+
+*****
+// neighbors' staffing as IV
+loc xvars dchrppd rnhrppd
+foreach v of varlist `xvars'{
+	bys tractid: egen nbr_`v' = total(`v')
+	replace nbr_`v' = nbr_`v' - `v'
+}
+
+
+gsort tractid
+
+save $adir/factract${testtag}.dta, replace
+use $adir/factract${testtag}.dta, clear
+export delim $adir/factract${testtag}, replace
